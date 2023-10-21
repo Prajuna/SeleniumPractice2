9fb27fc4c97d1c558ef32e72658045dfb66a243c
User <User@DESKTOP-ISPEK37>
2023-10-21 07:46:39 +0300

    alrt

--- seleniumPractice2/src/seleniumPractice2/alertPgm.java
+++ seleniumPractice2/src/seleniumPractice2/alertPgm.java
@@ -22,21 +22,17 @@ public class alertPgm {
 		 at.accept(); 
 		 
 		 //Alert with OK & Cancel
-		 driver.navigate().to("https://demo.automationtesting.in/Alerts.html");
 		 driver.findElement(By.linkText("Alert with OK & Cancel")).click();
 		 driver.findElement(By.xpath("//*[@id='CancelTab']/button")).click();
-		 Alert at1=wait1.until(ExpectedConditions.alertIsPresent());
-		 at1.dismiss();
+		 at.dismiss();
 		 String status=driver.findElement(By.id("demo")).getText();
 		 System.out.println(status);
 		 
 		 //Prompt
-		 driver.navigate().to("https://demo.automationtesting.in/Alerts.html");
 		 driver.findElement(By.linkText("Alert with Textbox")).click();
 		 driver.findElement(By.xpath("//div[@id='Textbox']/button")).click();
-		 Alert at2=wait1.until(ExpectedConditions.alertIsPresent());
-		 at2.sendKeys("Testdata");
-		 at2.accept();
+		 at.sendKeys("Testdata");
+		 at.accept();
 		 String status1=driver.findElement(By.id("demo1")).getText();
 		 System.out.println(status1);
 		 
